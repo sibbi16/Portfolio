0b560684b789f124cdd9d65e530c51163a27bcc9
Sibghat Ullah <sibghat@gitwork.co.uk>
2023-06-12 20:55:31 +0500

    Cv modified

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "../../Assets/../Assets/Sibghat_CV.pdf";
+  "https://raw.githubusercontent.com/sibbi16/portfolio/main/src/Assets/Sibghat_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
